d0f50520f2196f0e7eb80e0558d6cf132124f097
Alcot <amano_tooko@foxmail.com>
2017-03-08 23:55:56 +0800

    done 1101

---
+++ A/1101.cpp
@@ -0,0 +1,31 @@
+#include<cstdio>
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main()
+{
+    int i,j,k,a[100];
+    char x;
+    while(scanf("%d",&k)!=EOF)
+    {
+        a[0]=0;
+        i=0;
+        a[++i]=k;
+        while(scanf("%c",&x)!=EOF&&(x=='+'||x=='-'||x=='*'||x=='/'))
+        {
+            scanf("%d",&k);
+            if(x=='+')
+                a[++i]=k;
+            else if(x=='-')
+                a[++i]=-1*k;
+            else if(x=='*')
+                a[i]*=k;
+            else
+                a[i]/=k;
+        }
+        for(j=1; j<=i; j++)
+            a[0]+=a[j];
+        printf("%d\n",a[0]);
+    }
+    return 0;
+}
